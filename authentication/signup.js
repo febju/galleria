743412aaca0a64c20dad062e4127bdff39b91ecc
febju <felix.bjugg@gmail.com>
2017-01-24 15:31:43 +0200

    rekisteröinti korjattu

--- authentication/signup.js
+++ authentication/signup.js
@@ -7,39 +7,54 @@ module.exports = function(passport){
 	passport.use('signup', new LocalStrategy({
             passReqToCallback : true // allows us to pass back the entire request to the callback
         },
-        function(req, email, password, done) {
-
+        function(req, username, password) {
+			
+			var email = req.body.email;
+			
             findOrCreateUser = function(){
                 // find a user in Mongo with provided email
                 User.findOne({ 'email' :  email }, function(err, user) {
                     // In case of any error, return using the done method
                     if (err){
                         console.log('Error in SignUp: '+err);
-                        return done(err);
+                        return (err);
                     }
                     // already exists
                     if (user) {
-                        console.log('User already exists with email: '+email);
-                        return done(null, false, req.flash('error','Sähköpostiosoite on jo käytössä.'));
+                        console.log('User already exists with email: '+ email);
+                        return (null, false, req.flash('error','Sähköpostiosoite on jo käytössä.'));
                     } else {
-                        // if there is no user with that email
-                        // create the user
-                        var newUser = new User();
+                        User.findOne({ 'username' :  username }, function(err, user) {
+							// In case of any error, return using the done method
+							if (err){
+								console.log('Error in SignUp: '+err);
+								return (err);
+							}
+							// already exists
+							if (user) {
+								console.log('Username ' + username + ' is taken.');
+								return (null, false, req.flash('error','Nimimerkki on jo käytössä.'));
+							} else {
+								// if there is no user with that email
+								// create the user
+								var newUser = new User();
 
-                        // set the user's local credentials
-                        newUser.username = username;
-                        newUser.password = createHash(password);
-                        newUser.email = req.param('email');
+								// set the user's local credentials
+								newUser.username = username;
+								newUser.password = createHash(password);
+								newUser.email = email;
 
-                        // save the user
-                        newUser.save(function(err) {
-                            if (err){
-                                console.log('Error in Saving user: '+err);  
-                                throw err;  
-                            }
-                            console.log('User Registration succesful');    
-                            return done(null, newUser);
-                        });
+								// save the user
+								newUser.save(function(err) {
+									if (err){
+										console.log('Error in Saving user: '+err);  
+										throw err;  
+									}
+									console.log('User Registration succesful');    
+									return (null, newUser);
+								});
+							}
+						});
                     }
                 });
             };
