dfda913ce7d3899b0ffaf738eec4cd7a672e41e0
febju <felix.bjugg@gmail.com>
2017-01-29 23:44:42 +0200

    dev ja prod ympäristö eriytetty. Production: npm run-script production

--- models/db.js
+++ models/db.js
@@ -1,2 +1,14 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/galleria');
+var config = require('config');
+
+var env = config.util.getEnv('NODE_ENV');
+
+if (env === 'development') {
+	mongoose.connect('mongodb://localhost/galleria');
+}
+if (env === 'test') {
+	mongoose.connect('mongodb://localhost/galleria_test');
+}
+if (env === 'production') {
+	mongoose.connect('mongodb://localhost/galleria_production');
+}
